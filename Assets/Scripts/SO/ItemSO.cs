af8bee2be66838dc1fd2b6560c5b6ce5f9ac369a
hanabi0981 <84300859+hanabi0981@users.noreply.github.com>
2021-06-02 13:05:27 +0900

    Scriptable Object
    Scriptable Object Edit

---
+++ Assets/Scripts/SO/ItemSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public string value;
+    public Sprite sprite;
+    public float percent;
+}
+
+[CreateAssetMenu(fileName ="ItemSO", menuName ="Scriptable Object/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    public Item[] items;
+}
