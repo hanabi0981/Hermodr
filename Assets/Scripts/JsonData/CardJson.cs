39234fa9a0517dc67621921f66a7b6cb4c6b8bd6
kimjaehyun <84560732+JuM0ng@users.noreply.github.com>
2021-06-03 18:19:59 +0900

    JsonData

---
+++ Assets/Scripts/JsonData/CardJson.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CardJson : MonoBehaviour
+{
+    public CardData carddata;
+
+    [ContextMenu("To Json Data")]
+    void SaveUnitDataToJson()
+    {
+        string CardJsonData = JsonUtility.ToJson(carddata, true);
+        string path = Path.Combine(Application.dataPath, "CardData.json");
+        File.WriteAllText(path, CardJsonData);
+    }
+
+    [ContextMenu("From Json Data")]
+    void LoadUnitDataToJson()
+    {
+        string path = Path.Combine(Application.dataPath, "CardData.Json");
+        string CardJsonData = File.ReadAllText(path);
+        carddata = JsonUtility.FromJson<CardData>(CardJsonData);
+    }
+}
+
+
+[System.Serializable]
+public class CardData
+{
+    public string CardCode;
+    public string CardName;
+    public string CardExplain;
+    public int CardRune;
+    public int Cardgem;
+    public int CardDamage;
+    public int CardHeal;
+}
---
+++ Assets/Scripts/JsonData/ItemJson.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ItemJson : MonoBehaviour
+{
+    public ItemData itemdata;
+
+    [ContextMenu("To Json Data")]
+    void SaveUnitDataToJson()
+    {
+        string ItemJsonData = JsonUtility.ToJson(itemdata, true);
+        string path = Path.Combine(Application.dataPath, "ItemData.json");
+        File.WriteAllText(path, ItemJsonData);
+    }
+
+    [ContextMenu("From Json Data")]
+    void LoadUnitDataToJson()
+    {
+        string path = Path.Combine(Application.dataPath, "ItemData.Json");
+        string ItemJsonData = File.ReadAllText(path);
+        itemdata = JsonUtility.FromJson<ItemData>(ItemJsonData);
+    }
+}
+
+
+[System.Serializable]
+public class ItemData
+{
+    public string ItemCode;
+    public string ItemName;
+    public int ItemHP;
+    public int ItemLevel;
+    public int ItemATK;
+    public int ItemDEF;
+    public int ItemSPEED;
+    public int ItemEXP;
+    public int ItemATKSPEED;
+}
