4871806a4355d42a13587002ab0153e643944ac9
Bumzin Yoo <bluerain894@gmail.com>
2021-09-21 18:32:05 +0900

    Main Scene에 간단한 상점 시스템 구현

---
+++ Assets/Scripts/Use in MainScene/ButtonInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public Text PriceText;
+    public Text QuantityText;
+    public GameObject ShopManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PriceText.text = "Price : $" + ShopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
+        QuantityText.text = ShopManager.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+    }
+}
