72feab43958b7e60763e2508731d65a800e481b3
hanabi0981 <84300859+hanabi0981@users.noreply.github.com>
2021-11-14 18:43:30 +0900

    Revert "Revert "Revert "gitgit"""
    This reverts commit 7520c97002318d7b140d210d1e0aeb8af7be0593.

--- Assets/Scripts/Use in BattleScene/ObjectGenerator.cs
+++ Assets/Scripts/Use in BattleScene/ObjectGenerator.cs
@@ -1,32 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ObjectGenerator : MonoBehaviour
 {
     public GameObject player;
     public GameObject enemy;
-    public GameObject[] playerItems = new GameObject[6];
-    public int killcount = 0;
-
-    private void Start()
-    {
-        for (int i = 1; i < playerItems.Length; i++)
-        {
-            // InGameShopManager가 정상적으로 DontDestroyOnLoad에 존재한다면, 
-            if (InGameShopManager.instance != null)
-            {
-                // playerItems의 sprite를, 구매된 sprite의 index 값을 가져와 설정.
-                playerItems[i].GetComponent<Image>().sprite = InGameShopManager.instance.iL.ISprite[InGameShopManager.instance.HaveItemSpriteNumber[i]];
-                // 초기상태는 전부 비활성이므로, sprite가 변경된 gameobject만 활성화.
-                if (playerItems[i].GetComponent<Image>().sprite != null)
-                {
-                    playerItems[i].SetActive(true);
-                }
-            }
-        }
-    }
 
     // Update is called once per frame
     void Update()
