c8a97da9426e69408ef810715983109b4e2fd9aa
Bumzin Yoo <bluerain894@gmail.com>
2021-11-22 00:09:41 +0900

    Build Test

--- Assets/Scripts/Use in BattleScene/ObjectGenerator.cs
+++ Assets/Scripts/Use in BattleScene/ObjectGenerator.cs
@@ -12,32 +12,24 @@ public class ObjectGenerator : MonoBehaviour
 
     public static int killcount = 0;
 
+    List<string> HaveItemNumber = InGameShopManager.HaveItemSpriteNumber2;
+    List<string> HaveItemForgeNumber = InGameShopManager.HaveItemForgeNumber;
     private void Start()
     {
-        //for (int i = 1; i < playerItems.Length; i++)
-        //{
-        //    // InGameShopManager가 정상적으로 DontDestroyOnLoad에 존재한다면, 
-        //    if (InGameShopManager.instance != null)
-        //    {
-        //        // playerItems의 sprite를, 구매된 sprite의 index 값을 가져와 설정.
-        //        playerItems[i].GetComponent<Image>().sprite = InGameShopManager.instance.iL.ISprite[InGameShopManager.instance.HaveItemSpriteNumber[i]];
-        //        // 초기상태는 전부 비활성이므로, sprite가 변경된 gameobject만 활성화.
-        //        if (playerItems[i].GetComponent<Image>().sprite != null)
-        //        {
-        //            playerItems[i].SetActive(true);
-        //        }
-        //    }
-        //}
-        
-        List<string> HaveItemNumber = InGameShopManager.HaveItemSpriteNumber2;
-        List<string> HaveItemForgeNumber = InGameShopManager.HaveItemForgeNumber;
         for (int i = 1; i < HaveItemNumber.Count; i++)
         {
-            // Debug.Log("선택된 아이템 넘버 : " + PlayerPrefs.GetInt(HaveItemNumber[i]));
+            Debug.Log("선택된 아이템 넘버 : " + PlayerPrefs.GetInt(HaveItemNumber[i]));
             playerItems[i].GetComponent<Image>().sprite = GetComponent<ItemsList>().ISprite[PlayerPrefs.GetInt(HaveItemNumber[i])];
-            playerItems[i].GetComponentInChildren<Text>().text = "+"+PlayerPrefs.GetInt(HaveItemForgeNumber[i]);           
-
+            playerItems[i].GetComponentInChildren<Text>().text = "+" + PlayerPrefs.GetInt(HaveItemForgeNumber[i]);
+            playerItems[i].GetComponent<ItemsList>().ItemAbility(PlayerPrefs.GetInt(HaveItemNumber[i]));
         }
+        float damage = player.GetComponent<PlayerCombat>().damage;
+        float attackRange = player.GetComponent<PlayerCombat>().attackRange;
+        float timeBetAttack = player.GetComponent<PlayerCombat>().timeBetAttack;
+        float startHealth = player.GetComponent<PlayerCombat>().startHealth;
+        float moveSpeed = player.GetComponent<PlayerCombat>().moveSpeed;
+        Debug.Log("플레이어의 공격력 : " + damage + "\n" +
+                    "플레이어의 공격범위 : " + attackRange + "\n");
         SpawnEnemy();
         Invoke("SpawnEnemy", 2);
 
@@ -63,6 +55,7 @@ public class ObjectGenerator : MonoBehaviour
             Invoke("MainLoad", 2);
             killcount = 0;
         }
+        
     }
 
     void SpawnEnemy()
--- Assets/Scripts/Use in InGameStore/ItemsList.cs
+++ Assets/Scripts/Use in InGameStore/ItemsList.cs
@@ -6,7 +6,98 @@ public class ItemsList : MonoBehaviour
 {
     public Sprite[] ISprite = new Sprite[11];
     public int[] IPrice = new int[11];
-
-
-    
+    public int[] Ability = new int[11] { 0, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 }; 
+    public void ItemAbility(int index)
+    {
+        switch (index)
+        {
+            case 1:
+                Ability_01();
+                break;
+            case 2:
+                Ability_02();
+                break;
+            case 3:
+                Ability_03();
+                break;
+            case 4:
+                Ability_04();
+                break;
+            case 5:
+                Ability_05();
+                break;
+            case 6:
+                Ability_06();
+                break;
+            case 7:
+                Ability_07();
+                break;
+            case 8:
+                Ability_08();
+                break;
+            case 9:
+                Ability_09();
+                break;
+            case 10:
+                Ability_10();
+                break;
+            default:
+                break;
+        }
+    }
+    private void Ability_01()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().damage += 10.0f;
+        og.player.GetComponent<PlayerCombat>().timeBetAttack -= 0.2f;
+    }
+    private void Ability_02()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().damage += 5.0f;
+    }
+    private void Ability_03()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().attackRange += 0.2f;
+    }
+    private void Ability_04()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().moveSpeed = 3.0f;
+    }
+    private void Ability_05()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().startHealth += 10f;
+    }
+    private void Ability_06()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().damage += 20.0f;
+        og.player.GetComponent<PlayerCombat>().timeBetAttack -= 0.4f;
+    }
+    private void Ability_07()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().startHealth -= 10.0f;
+        og.player.GetComponent<PlayerCombat>().moveSpeed += 5;
+    }
+    private void Ability_08()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().startHealth += 20.0f;
+        og.player.GetComponent<PlayerCombat>().moveSpeed -= 5.0f;
+    }
+    private void Ability_09()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().damage += 10.0f;
+        og.player.GetComponent<PlayerCombat>().startHealth -= 10.0f;
+    }
+    private void Ability_10()
+    {
+        ObjectGenerator og = GameObject.FindObjectOfType<ObjectGenerator>();
+        og.player.GetComponent<PlayerCombat>().timeBetAttack -= 0.1f;
+    }
 }
