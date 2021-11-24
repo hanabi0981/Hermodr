097665b96dffdd964479f0cade8a19378d0ad95b
Bumzin Yoo <bluerain894@gmail.com>
2021-11-24 22:35:54 +0900

    카드 사용 애니메이션 추가 Logic : target 이 되는 오브젝트에 자식 컴포넌트로 Instantiate 되었다가, 일정 시간 후 파괴됨. - Player Prefab 소환 > Bless - 화살 모양 카드 > LightningStorm - 회복 카드 > Heal - 속박 카드 > Bind

--- Assets/Scripts/Use in CardSystem/DragDrop.cs
+++ Assets/Scripts/Use in CardSystem/DragDrop.cs
@@ -11,6 +11,11 @@ public class DragDrop : MonoBehaviour
     public GameObject Card4;
     public GameObject Card5;
 
+    public GameObject Effect_01;
+    public GameObject Effect_02;
+    public GameObject Effect_03;
+    public GameObject Effect_04;
+
     public GameObject Player;
     public GameObject Enemy;
 
@@ -67,30 +72,49 @@ public class DragDrop : MonoBehaviour
 
             if (this.gameObject==Card1||this.gameObject==Card2)
             {
-                Destroy(this.gameObject);
                 DrawCards.handCount--;
-                Instantiate(Player);
+
+                Vector3 localPosition = GetComponent<RectTransform>().position;
+                GetComponent<RectTransform>().position = localPosition + new Vector3(0, 0, -1000);
+
+                GameObject p = Instantiate(Player);
+                Effect_01.transform.position = Player.transform.position;
+                Effect_01.transform.position -= new Vector3(-0.8f, 1.14f, 0f);
+
+                StartCoroutine(CardEffect(Effect_01, p, 0.6f));
             }
             else if (this.gameObject == Card3)
             {
-                Destroy(this.gameObject);
                 DrawCards.handCount--;
+
+                Vector3 localPosition = GetComponent<RectTransform>().position;
+                GetComponent<RectTransform>().position = localPosition + new Vector3(0, 0, -1000);
+
                 target = GameObject.FindGameObjectsWithTag("Enemy");
                 float damage = target[0].GetComponent<EnemyCombat>().startHealth / 8;
                 for(int i = 0; i < target.Length; i++)
                 {
                     target[i].GetComponent<EnemyCombat>().OnDamage(damage);
+                    Effect_02.transform.position = target[i].transform.position;
+                    Effect_02.transform.position -= new Vector3(-1.0f, 0.6f, 0f);
+                    StartCoroutine(CardEffect(Effect_02, target[i], 0.9f));
                     target[i] = null;
                 }
             }
             else if(this.gameObject == Card4)
             {
-                Destroy(this.gameObject);
                 DrawCards.handCount--;
+
+                Vector3 localPosition = GetComponent<RectTransform>().position;
+                GetComponent<RectTransform>().position = localPosition + new Vector3(0, 0, -1000);
+
                 target = GameObject.FindGameObjectsWithTag("Player");
                 for(int i = 0; i <target.Length; i++)
                 {
                     target[i].GetComponent<PlayerCombat>().RestoreHearth(50.0f);
+                    Effect_03.transform.position = target[i].transform.position;
+                    Effect_03.transform.position -= new Vector3(-0.8f, 1.14f, 0f);
+                    StartCoroutine(CardEffect(Effect_03, target[i], 0.7f));
                     target[i] = null;
                 }           
             }
@@ -99,7 +123,6 @@ public class DragDrop : MonoBehaviour
                 DrawCards.handCount--;
                 Vector3 localPosition = GetComponent<RectTransform>().position;
                 GetComponent<RectTransform>().position = localPosition + new Vector3(0, 0, -1000);
-
                 StartCoroutine(Card5_Delay(2.0f));
             }
 
@@ -119,14 +142,32 @@ public class DragDrop : MonoBehaviour
             {
                 normalSpeeds[i] = target[i].GetComponent<EnemyCombat>().moveSpeed;
                 target[i].GetComponent<EnemyCombat>().moveSpeed = 0;
+
+                Effect_04.transform.position = target[i].transform.position;
+                Effect_04.transform.position -= new Vector3(-1.3f, 0.7f, 0f);
+                GameObject e = Instantiate(Effect_04);
+                e.transform.SetParent(target[i].transform);
             }
             yield return new WaitForSeconds(delay);
+
             for (int i = 1; i < target.Length; i++)
             {
                 target[i].GetComponent<EnemyCombat>().moveSpeed = normalSpeeds[i];
+                Destroy(target[i].transform.GetChild(2).gameObject);
                 target[i] = null;
             }
         }
-        Destroy(gameObject);
+        Destroy(this.gameObject);
+    }
+    IEnumerator CardEffect(GameObject effect, GameObject target, float delay)
+    {
+
+        GameObject e = Instantiate(effect);
+        e.transform.SetParent(target.transform);
+       
+        yield return new WaitForSeconds(delay);
+
+        Destroy(target.transform.GetChild(2).gameObject);
+        Destroy(this.gameObject);
     }
 }
